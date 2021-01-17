058fd33114cc3d6265fe4f23dfca521a5f2b8879
Katsusuke Shimizu <katsusuke@gmail.com>
2021-01-17 11:37:51 +0900

    suppress errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::time::Duration;
-use surf;
 use getopts::Options;
 use async_std::task;
 use chrono::prelude::*;
@@ -19,37 +18,29 @@ fn message_for_rainfail(w: &yahooapi::Weather) -> String {
     return message;
 }
 
-async fn check_rainfall(appid: &str, coordinates: &str, slack_token: &str) -> surf::Result<()> {
-    if let Some(w) = yahooapi::find_rainfail(appid, coordinates).await? {
+async fn check_rainfall(appid: &str, coordinates: &str, slack_token: &str) -> Duration {
+    let default_wait = Duration::from_secs(60 * 10);
+    if let Ok(Some(w)) = yahooapi::find_rainfail(appid, coordinates).await {
         let message = message_for_rainfail(&w);
-        slackapi::post2slack(slack_token, message).await?;
+        let _ = slackapi::post2slack(slack_token, message).await;
+        let now = Local::now().naive_utc();
+        let duration = (w.date + chrono::Duration::seconds(60 * 60 * 6)) - now;
+        duration.to_std().unwrap_or(default_wait)
+    } else {
+        default_wait
     }
-    Ok(())
 }
 
-async fn watch(appid: &str, coordinates: &str, slack_token: &str) -> surf::Result<()> {
+async fn watch(appid: &str, coordinates: &str, slack_token: &str) -> Duration {
     loop {
-        if let Some(w) = yahooapi::find_rainfail(appid, coordinates).await? {
-            let message = message_for_rainfail(&w);
-            slackapi::post2slack(slack_token, message).await?;
-            let now = Local::now().naive_utc();
-            let duration = (w.date + chrono::Duration::seconds(60 * 60 * 6)) - now;
-            if let Ok(std_duration) = duration.to_std() {
-                println!("sleep");
-                task::sleep(std_duration).await;
-            } else {
-                println!("sleep");
-                task::sleep(Duration::from_secs(60 * 10)).await;
-            }
-        } else {
-            println!("sleep");
-            task::sleep(Duration::from_secs(60 * 10)).await;
-        }
+        let wait = check_rainfall(appid, coordinates, slack_token).await;
+        println!("sleep: {}secs", wait.as_secs());
+        task::sleep(wait).await;
     }
 }
 
 #[async_std::main]
-async fn main() -> surf::Result<()> {
+async fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     opts.optopt("i", "appid", "Yahho! JAPAN appid(Required)", "APPID");
@@ -66,19 +57,17 @@ async fn main() -> surf::Result<()> {
     if m.opt_present("h") {
         let program = args[0].clone();
         print_usage(&program, opts);
-        return Ok(());
+        return;
     }
 
     match (m.opt_str("i"), m.opt_str("c"), m.opt_str("s")) {
         (Some(appid), Some(slack_token), Some(coordinates)) => {
             if m.opt_present("w") {
-                watch(&appid, &slack_token, &coordinates).await?
+                watch(&appid, &slack_token, &coordinates).await;
             } else {
-                check_rainfall(&appid, &slack_token, &coordinates).await?
+                check_rainfall(&appid, &slack_token, &coordinates).await;
             }
         },
         _ => println!("No appid"),
     }
-    
-    Ok(())
 }
