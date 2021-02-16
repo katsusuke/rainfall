6bce62f9c8676634072281dad72f05e25d35a123
Katsusuke Shimizu <katsusuke@gmail.com>
2021-02-16 11:23:19 +0900

    use new api

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ fn print_usage(program: &str, opts: Options) {
 fn split_coordinates(coordinates: &str) -> Option<(&str, &str)> {
     let a = coordinates.split(',').collect::<Vec<&str>>();
     if let &[lat, lon] = &a[..] {
-        return Some((lat, lon));
+        return Some((lon, lat));
     } else {
         return None;
     }
 }
 
 fn yahoo_url(coordinates: &str, date: &str) -> String {
-    if let Some((lat, lon)) = split_coordinates(coordinates) {
+    if let Some((lon, lat)) = split_coordinates(coordinates) {
         return format!("https://weather.yahoo.co.jp/weather/zoomradar/?lat={}&lon={}&z=15&t={}", lat.to_string(), lon.to_string(), date.to_string());
     } else {
         return "".to_string();
@@ -36,11 +36,11 @@ fn message_for_rainfail(w: &yahooapi::Weather, coordinates: &str) -> String {
     return message;
 }
 
-async fn check_rainfall(appid: &str, coordinates: &str, slack_token: &str, channel: &str) -> Duration {
+async fn check_rainfall(appid: &str, coordinates: &str, slack_url: &str) -> Duration {
     let default_wait = Duration::from_secs(60 * 10);
     if let Ok(Some(w)) = yahooapi::find_rainfail(appid, coordinates).await {
         let message = message_for_rainfail(&w, coordinates);
-        let _ = slackapi::post2slack(slack_token, channel, message).await;
+        let _ = slackapi::post2slack(slack_url, message).await;
         let now = Local::now().naive_utc();
         let duration = (w.date + chrono::Duration::seconds(60 * 60 * 6)) - now;
         duration.to_std().unwrap_or(default_wait)
@@ -49,9 +49,9 @@ async fn check_rainfall(appid: &str, coordinates: &str, slack_token: &str, chann
     }
 }
 
-async fn watch(appid: &str, coordinates: &str, slack_token: &str, channel: &str) -> Duration {
+async fn watch(appid: &str, coordinates: &str, slack_url: &str) -> Duration {
     loop {
-        let wait = check_rainfall(appid, coordinates, slack_token, channel).await;
+        let wait = check_rainfall(appid, coordinates, slack_url).await;
         println!("sleep: {}secs", wait.as_secs());
         task::sleep(wait).await;
     }
@@ -62,17 +62,16 @@ async fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     opts.optopt("i", "appid", "Yahho! JAPAN appid(Required)", "APPID");
-    opts.optopt("c", "coordinates", "latitude,longitude(Required)", "LATITUDE,LONGITUDE");
-    opts.optopt("s", "slack-token", "Slack Token(Required)", "TOKEN");
-    opts.optopt("C", "slack-channel", "Slack Channel(default: #weather)", "TOKEN");
+    opts.optopt("c", "coordinates", "latitude,longitude(Required)", "LONGITUDE,LATITUDE");
+    opts.optopt("s", "slack-url", "Slack Incomming Webhook URL(Required)", "WEBHOOK_URL");
     opts.optflag("w", "watch", "Service mode");
     opts.optflag("h", "help", "print this help menu");
-    
+
     let m = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
-    
+
     if m.opt_present("h") {
         let program = args[0].clone();
         print_usage(&program, opts);
@@ -80,12 +79,11 @@ async fn main() {
     }
 
     match (m.opt_str("i"), m.opt_str("c"), m.opt_str("s")) {
-        (Some(appid), Some(slack_token), Some(coordinates)) => {
-            let channel = m.opt_str("C").unwrap_or("#weather".to_string());
+        (Some(appid), Some(slack_url), Some(coordinates)) => {
             if m.opt_present("w") {
-                watch(&appid, &slack_token, &channel, &coordinates).await;
+                watch(&appid, &slack_url, &coordinates).await;
             } else {
-                check_rainfall(&appid, &slack_token, &channel, &coordinates).await;
+                check_rainfall(&appid, &slack_url, &coordinates).await;
             }
         },
         _ => println!("No required options"),
--- src/slackapi.rs
+++ src/slackapi.rs
@@ -3,25 +3,22 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 struct SlackPostMessagePayload {
-    token: String,
-    channel: String,
     text: String,
     icon_emoji: String,
     username: String
 }
 
-pub async fn post2slack(token: impl Into<String>, channel: impl Into<String>, text: impl Into<String>) -> surf::Result<()> {
-    let url = "https://slack.com/api/chat.postMessage";
+pub async fn post2slack(url: impl Into<String>, text: impl Into<String>) -> surf::Result<()> {
     let body = SlackPostMessagePayload {
-        token: token.into(),
-        channel: channel.into(),
         text: text.into(),
         icon_emoji: ":rain_cloud:".into(),
         username: "雨ですよBot".into()
     };
-    let mime = surf::http::Mime::from_str("application/json; charset=utf-8").unwrap();
-    let data = surf::post(url)
-        .query(&body)?
+    let body_str = serde_json::to_string(&body).unwrap();
+    let u = url.into();
+    let mime = surf::http::Mime::from_str("application/json").unwrap();
+    let data = surf::post(u)
+        .body(body_str)
         .content_type(mime)
         .recv_string().await?;
     println!("data: {}", data);
